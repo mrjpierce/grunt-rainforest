7b1a4720c1c1cbdb164c6b3ec230c1bdc3f6acd5
James Pierce <james.pierce@liveauctioneers.com>
2016-04-01 17:04:41 -0600

    Completed dev work, untested

--- tasks/rainforest.js
+++ tasks/rainforest.js
@@ -8,43 +8,79 @@
 
 'use strict';
 
+var shell = require('shelljs');
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    grunt.registerMultiTask('rainforest', 'A grunt plugin for running rainforest QA tasks', function() {
+        var options = this.options({
+            apiBaseUrl: 'https://app.rainforestqa.com',
+            token: '',
+            tag: '',
+            browsers: []
+            upload: false
+        });
 
-  grunt.registerMultiTask('rainforest', 'A grunt plugin for running rainforest QA tasks', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+        if(options.apiBaseUrl === '') {
+            shell.echo('ERROR: \'apiBaseUrl\' option can not be empty');
+            return false;
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
+        if(options.token === '') {
+            shell.echo('ERROR: \'token\' option can not be empty');
+            return false;
+        }
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+        var tokenString = ' --token ' + options.token;
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+        // Check for rainforest gem install
+        shell.echo('Detecting rainforest gem...');
+
+        var checkResult = shell.exec('rainforest', {silent:true});
+        if(checkResult.code !== 0) {
+            shell.echo('ERROR: Rainforest gem is not installed! :(');
+            shell.echo('Install with: \'gem install rainforest-cli\'');
+            return false;
+        }
+
+        shell.echo('Rainforest gem detected :) \n');
+
+        // Upload tests
+        if(options.upload) {
+            shell.echo('Uploading tests...');
+            var uploadResult = shell.exec('rainforest upload' + tokenString);
+            if(uploadResult.code !== 0) {
+                shell.echo('ERROR: Upload did not complete successfully');
+                return false;
+            }
+        }
+
+        // Build run command
+        var runCmd = 'rainforest run'
+
+        if(options.tag !== '') {
+            runCmd += ' --tag ' + options.tag
+        } else {
+            runCmd += ' all';
+        }
+
+        if (typeof options.browsers !== 'undefined' && options.browsers.length > 0) {
+            runCmd += ' --browsers ';
+            options.browsers.forEach(function(browser, index) {
+                if(index !== 1)
+                    runCmd += ',';
+
+                runCmd += browser;
+            });
+        }
+
+        runCmd += tokenString;
+
+        var runResult = shell.exec(runCmd);
+
+        if(runResult.code !== 0) {
+            shell.echo('ERROR: Test run did not complete successfully');
+            return false;
+        }
     });
-  });
 
 };
